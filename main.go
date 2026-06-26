b788135a75678ec70e40be9cfabe460eebfa2395
WatchJani <jankokondic84@gmail.com>
2026-06-26 21:29:25 +0200

    read file

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Rating struct {
+	UserID    int
+	MovieID   int
+	Rating    float64
+	Timestamp string
+}
 
 func main() {
-	fmt.Println("yes")
+	file, err := os.Open("rating.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	var ratings []Rating
+
+	for {
+		record, err := reader.Read()
+
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		userID, _ := strconv.Atoi(record[0])
+		movieID, _ := strconv.Atoi(record[1])
+		rating, _ := strconv.ParseFloat(record[2], 64)
+
+		ratings = append(ratings, Rating{
+			UserID:    userID,
+			MovieID:   movieID,
+			Rating:    rating,
+			Timestamp: record[3],
+		})
+	}
+
+	fmt.Println("Broj ocjena:", len(ratings))
+	fmt.Println(ratings[0])
 }
