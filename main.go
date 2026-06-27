5417d43d4c3e65ef519eb7c493084d7eada86b06
WatchJani <jankokondic84@gmail.com>
2026-06-27 14:04:41 +0200

    reading model

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"log"
+	"root/constants"
 	"root/model"
 )
 
 func main() {
-	model.ModelRunner()
+	model, err := model.LoadModel(constants.ModelPath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	_ = model
 }
