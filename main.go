c94ab4dba7be48f1db6f57808eb8bf9b45463fae
WatchJani <jankokondic84@gmail.com>
2026-06-27 12:31:27 +0200

    latent factor

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"io"
+	"math/rand/v2"
 	"os"
 	"root/constants"
 	"strconv"
@@ -10,9 +11,11 @@ import (
 
 type Configuration struct {
 	NumberOfLatentFactors   int
-	LearningRate            int
-	RegularizationParameter int
+	LearningRate            float32
+	RegularizationParameter float32
 	NumberOfEpochs          int
+	InitializationMin       float64
+	InitializationMax       float64
 }
 
 type Engine struct {
@@ -22,16 +25,35 @@ type Engine struct {
 	Configuration
 }
 
-func New(data []constants.Rating) *Engine {
+func RandomFloat(min, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
+
+func New(data []constants.Rating, configuration Configuration) *Engine {
 	return &Engine{
-		User:   make(map[string][]float64),
-		Movies: make(map[string][]float64),
-		Data:   data,
+		User:          make(map[string][]float64),
+		Movies:        make(map[string][]float64),
+		Data:          data,
+		Configuration: configuration,
 	}
 }
 
-func (e *Engine) Init() {
+func (e *Engine) InitialLatentFactors() []float64 {
+	numberOfLatentFactor := e.NumberOfLatentFactors
 
+	latentFactor := make([]float64, numberOfLatentFactor)
+
+	for index := 0; index < numberOfLatentFactor; index++ {
+		latentFactor[index] = RandomFloat(e.InitializationMin, e.InitializationMax)
+	}
+
+	return latentFactor
+}
+
+func (e *Engine) Init() {
+	for _, record := range e.Data {
+
+	}
 }
 
 func main() {
@@ -78,6 +100,16 @@ func main() {
 		})
 	}
 
-	engine := New(rating)
+	conf := Configuration{
+		NumberOfLatentFactors:   20,
+		LearningRate:            0.01,
+		RegularizationParameter: 0.02,
+		NumberOfEpochs:          20,
+		InitializationMin:       -0.1,
+		InitializationMax:       0.1,
+	}
+
+	engine := New(rating, conf)
+	engine.Init()
 
 }
